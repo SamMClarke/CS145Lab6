047776fb81dfcb29d64a883bb3a287a84a38e5eb
Sam Clarke <SClarke4772@student.whatcom.edu>
2024-03-04 11:33:14 -0800

    got basic play() method working

---
+++ QuestionNode.java
@@ -0,0 +1,18 @@
+public class QuestionNode
+{
+    public String data;
+    public QuestionNode yes;
+    public QuestionNode no;
+
+    public QuestionNode(String data)
+    {
+        this(data, null, null);
+    }
+
+    public QuestionNode(String data, QuestionNode yes, QuestionNode no)
+    {
+        this.data = data;
+        this.yes = yes;
+        this.no = no;
+    }
+}
---
+++ QuestionTree.java
@@ -0,0 +1,95 @@
+import java.util.*;
+
+import javax.xml.bind.annotation.W3CDomHandler;
+
+import java.io.*;
+
+public class QuestionTree
+{
+    private QuestionNode overallRoot;
+    private UserInterface ui;
+    private int gamesPlayed = 0;
+    private int gamesWon = 0;
+
+    public QuestionTree(UserInterface ui)
+    {
+        overallRoot = new QuestionNode("computer");
+        this.ui = ui;
+    }
+
+    public void play()
+    {
+        play(overallRoot);
+    }
+
+    private void play(QuestionNode root)
+    {
+        if (root.yes == null && root.no == null)
+        {
+            //Base case
+            gamesPlayed++;
+            ui.print("Would your object happen to be " + root.data + "?");
+            if (ui.nextBoolean())
+            {
+                //Win
+                gamesWon++;
+                ui.println("I win!");
+            }
+            else
+            {
+                //Lose
+                ui.print("I lose. What is your object?");
+                String object = ui.nextLine();
+                ui.print("Type a yes/no question to distinguish your item from " + root.data + ":");
+                String question = ui.nextLine();
+                ui.print("And what is the answer for your object?");
+                boolean answer = ui.nextBoolean();
+
+                if (answer)
+                {
+                    root.yes = new QuestionNode(object);
+                    root.no = new QuestionNode(root.data);
+                }
+                else
+                {
+                    root.no = new QuestionNode(object);
+                    root.yes = new QuestionNode(root.data);
+                }
+                root.data = question;
+            }
+        }
+        else
+        {
+            //Recursive case
+            ui.print(root.data);
+            if (ui.nextBoolean())
+            {
+                play(root.yes);
+            }
+            else
+            {
+                play(root.no);
+            }
+        }
+    }
+
+    public void save(PrintStream output)
+    {
+        
+    }
+
+    public void load(Scanner input)
+    {
+
+    }
+
+    public int totalGames()
+    {
+        return gamesPlayed;
+    }
+
+    public int gamesWon()
+    {
+        return gamesWon;
+    }
+}
