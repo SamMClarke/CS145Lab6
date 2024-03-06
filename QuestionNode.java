3c50979f56e60aa8e5faf2011d6ead7ba4c2438e
WCCJtak <jtak9479@student.whatcom.edu>
2024-03-06 10:06:53 -0800

    trivial: Java Lint

--- QuestionNode.java
+++ QuestionNode.java
@@ -1,16 +1,13 @@
-public class QuestionNode
-{
+public class QuestionNode {
     public String data;
     public QuestionNode yes;
     public QuestionNode no;
 
-    public QuestionNode(String data)
-    {
+    public QuestionNode(String data) {
         this(data, null, null);
     }
 
-    public QuestionNode(String data, QuestionNode yes, QuestionNode no)
-    {
+    public QuestionNode(String data, QuestionNode yes, QuestionNode no) {
         this.data = data;
         this.yes = yes;
         this.no = no;
--- QuestionTree.java
+++ QuestionTree.java
@@ -1,139 +1,107 @@
 import java.util.*;
 import java.io.*;
 
-public class QuestionTree
-{
+public class QuestionTree {
     private QuestionNode overallRoot;
     private UserInterface ui;
     private int gamesPlayed = 0;
     private int gamesWon = 0;
 
-    public QuestionTree(UserInterface ui)
-    {
-        if (ui == null)
-        {
+    public QuestionTree(UserInterface ui) {
+        if (ui == null) {
             throw new IllegalArgumentException("UI parameter is null");
         }
         overallRoot = new QuestionNode("computer");
         this.ui = ui;
     }
 
-    public void play()
-    {
+    public void play() {
         play(overallRoot);
     }
 
-    private void play(QuestionNode root)
-    {
-        if (root.yes == null && root.no == null) //Leaf Node
-        {
-            //Base case
+    private void play(QuestionNode root) {
+        if (root.yes == null && root.no == null) { // Leaf Node
+            // Base case
             gamesPlayed++;
             ui.print("Would your object happen to be " + root.data + "?");
-            if (ui.nextBoolean())
-            {
-                //Win
+            if (ui.nextBoolean()) {
+                // Win
                 gamesWon++;
                 ui.println("I win!");
-            }
-            else
-            {
-                //Lose
+            } else {
+                // Lose
                 ui.print("I lose. What is your object?");
                 String object = ui.nextLine();
                 ui.print("Type a yes/no question to distinguish your item from " + root.data + ":");
                 String question = ui.nextLine();
                 ui.print("And what is the answer for your object?");
-                if (ui.nextBoolean())
-                {
+                if (ui.nextBoolean()) {
                     root.yes = new QuestionNode(object);
                     root.no = new QuestionNode(root.data);
-                }
-                else
-                {
+                } else {
                     root.no = new QuestionNode(object);
                     root.yes = new QuestionNode(root.data);
                 }
                 root.data = question;
             }
-        }
-        else
-        {
-            //Recursive case
+        } else {
+            // Recursive case
             ui.print(root.data);
-            if (ui.nextBoolean())
-            {
+            if (ui.nextBoolean()) {
                 play(root.yes);
-            }
-            else
-            {
+            } else {
                 play(root.no);
             }
         }
     }
 
-    public void save(PrintStream output)
-    {
-        if (output == null)
-        {
+    public void save(PrintStream output) {
+        if (output == null) {
             throw new IllegalArgumentException("Output parameter is null");
         }
         save(output, overallRoot);
     }
 
-    private void save(PrintStream output, QuestionNode root)
-    {
-        if (root.yes == null && root.no == null)
-        {
-            //Base case
+    private void save(PrintStream output, QuestionNode root) {
+        if (root.yes == null && root.no == null) {
+            // Base case
             output.println("A:" + root.data);
-        }
-        else
-        {
-            //Recursive case
+        } else {
+            // Recursive case
             output.println("Q:" + root.data);
             save(output, root.yes);
             save(output, root.no);
         }
     }
 
-    public void load(Scanner input)
-    {
-        if (input == null)
-        {
+    public void load(Scanner input) {
+        if (input == null) {
             throw new IllegalArgumentException("Input parameter is null");
         }
         overallRoot = loadHelper(input);
     }
 
-    private QuestionNode loadHelper(Scanner input)
-    {
+    private QuestionNode loadHelper(Scanner input) {
         QuestionNode root;
-        if (!input.hasNextLine())
-        {
+        if (!input.hasNextLine()) {
             return null;
         }
         String nextLine = input.nextLine();
-        if (nextLine.charAt(0) == 'Q')
-        {
+        if (nextLine.charAt(0) == 'Q') {
             root = new QuestionNode(nextLine.substring(2));
             root.yes = loadHelper(input);
             root.no = loadHelper(input);
-        }
-        else
-        {
+        } else {
             root = new QuestionNode(nextLine.substring(2));
         }
         return root;
     }
 
-    public int totalGames()
-    {
+    public int totalGames() {
         return gamesPlayed;
     }
 
-    public int gamesWon()
-    {
+    public int gamesWon() {
         return gamesWon;
     }
 }
